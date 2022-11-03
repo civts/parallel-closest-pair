ac741bab7e1fee2fd30f2de180dd64e0d533795f
Dagu9 <davide.guidolin99@gmail.com>
2022-11-03 10:17:19 +0100

    Start implementation of Divide and Conquer algorithm➗➕

--- serial/main.c
+++ serial/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     int x, y;
@@ -42,12 +43,43 @@ PointVec load_data(char *path) {
     return point_vec;
 }
 
+// Compare functions used to sort the points
+int compare_x(const void *e1, const void *e2) {
+    Point *p1 = (Point *)e1;
+    Point *p2 = (Point *)e2;
+    return (p1->x - p2->x);
+}
+
+int compare_y(const void *e1, const void *e2) {
+    Point *p1 = (Point *)e1;
+    Point *p2 = (Point *)e2;
+    return (p1->y - p2->y);
+}
+
+// Compute distance between 2 points
+float distance(Point p1, Point p2) {
+    return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
+}
+
+float closestPoints(Point* points, int n) {
+    // Base case here
+
+    int mid = n/2;
+
+    // Split array
+    // Find left and right minimum distance
+    // Check points in the middle
+}
+
 int main() {
     int i;
     PointVec point_vec;
 
-    point_vec = load_data("data/test_dataset.txt");
+    point_vec = load_data("../data/test_dataset.txt");
     
+    // Sort array based on x coordinate
+    qsort(point_vec.points, point_vec.length, sizeof(Point), compare_x);
+
     for (i = 0; i<point_vec.length; i++) {
         printf("%d %d\n", point_vec.points[i].x, point_vec.points[i].y);
     }
