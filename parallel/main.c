081f0b70d7157e4813e49636e885a92f8393da1e
Guidolin <davide.guidolin@studenti.unitn.it>
2022-11-22 23:18:48 +0100

    Get global distance using local distances

--- parallel/main.c
+++ parallel/main.c
@@ -7,12 +7,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+// // Custom min function used in the reduce
+// void minPairOfPoints(PairOfPoints *in, PairOfPoints *out, int *len, MPI_Datatype *dptr) {
+//   int i;
+//   PairOfPoints min = in[0];
+
+//   printf("len %d\n", *len);
+
+//   for (i = 0; i < *len; i++) {
+//     printf("d %.2f\n", in[i].distance);
+//     if (in[i].distance < min.distance) {
+//       min.distance = in[i].distance;
+//       min.point1 = in[i].point1;
+//       min.point2 = in[i].point2;
+//     }
+
+//     out[i] = min;
+//   }
+// }
+
 int main(int argc, char **argv) {
   int comm_sz;
   int my_rank;
   int n_points;
   int local_n;
   int i;
+  double total_time, read_time;
+
   PointVec point_vec;
   PointVec local_points;
   PairOfPoints local_d;
@@ -32,9 +53,29 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
+  // MPI initializations
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  total_time = -MPI_Wtime();
+
+  // // Create min operation used in reduce
+  // MPI_Op min_op;
+  // MPI_Op_create((MPI_User_function*)minPairOfPoints, 1, &min_op);
+  MPI_Status mpi_stat;
+
+  // Create Point Datatype for MPI
+  MPI_Datatype types[2] = {MPI_INT, MPI_INT};
+  MPI_Datatype mpi_point_type;
+
+  MPI_Aint offsets[2];
+  offsets[0] = offsetof(Point, x);
+  offsets[1] = offsetof(Point, y);
+  const int blocklengths[2] = {1, 1};
+
+  MPI_Type_create_struct(2, blocklengths, offsets, types, &mpi_point_type);
+  MPI_Type_commit(&mpi_point_type);
 
   // setup output file
   int num_digits = 0;
@@ -50,18 +91,7 @@ int main(int argc, char **argv) {
   fprintf(out_fp, "Process %d\n", my_rank);
   free(rank_str);
 
-  // Create Point Datatype for MPI
-  MPI_Datatype types[2] = {MPI_INT, MPI_INT};
-  MPI_Datatype mpi_point_type;
-
-  MPI_Aint offsets[2];
-  offsets[0] = offsetof(Point, x);
-  offsets[1] = offsetof(Point, y);
-  const int blocklengths[2] = {1, 1};
-
-  MPI_Type_create_struct(2, blocklengths, offsets, types, &mpi_point_type);
-  MPI_Type_commit(&mpi_point_type);
-
+  read_time = -MPI_Wtime();
   if (my_rank == 0) {
 
     // load data
@@ -75,6 +105,7 @@ int main(int argc, char **argv) {
 
     fprintf(out_fp, "Loaded and sorted %d points\n", n_points);
   }
+  read_time += MPI_Wtime;
 
   // Broadcast number of points to each process
   MPI_Bcast(&n_points, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -83,6 +114,7 @@ int main(int argc, char **argv) {
 
   int *displs = (int *)malloc(comm_sz * sizeof(int));
   int *scounts = (int *)malloc(comm_sz * sizeof(int));
+  Point *borders = (Point *)malloc((comm_sz - 1) * sizeof(Point));
 
   int stride = n_points / comm_sz;
 
@@ -118,12 +150,33 @@ int main(int argc, char **argv) {
     local_d = detClosestPointsWrapper(local_points);
     fprintf(out_fp, "Local smallest distance: %.2f\n", local_d.distance);
   } else {
+    local_d.distance = DBL_MAX;
     fprintf(out_fp, "Less than 2 points: no distance\n");
   }
+  
+  // Reduce
+  // MPI_Reduce(&local_d, &global_d, 1, mpi_point_type, min_op, 0, MPI_COMM_WORLD);
 
-  // TODO: Send local_d to process 0
+  // TO improve: tree merge
+  if (my_rank == 0) {
+    global_d = local_d;
+    for (i = 1; i < comm_sz; i++) {
+      MPI_Recv(&local_d, 1, mpi_point_type, i, i, MPI_COMM_WORLD, &mpi_stat);
+      if (local_d.distance < global_d.distance) {
+        global_d.distance = local_d.distance;
+        global_d.point1 = local_d.point1;
+        global_d.point2 = local_d.point2;
+      }
+    }
+  } else {
+    MPI_Send(&local_d, 1, mpi_point_type, 0, my_rank, MPI_COMM_WORLD);
+  }
+
   // Check intermediate points
-  // if (my_rank == 0) {
+  if (my_rank == 0) {
+    fprintf(out_fp, "global_d %.2f, P1 (%d, %d), P2 (%d, %d)\n", global_d.distance, 
+            global_d.point1.x, global_d.point1.y,
+            global_d.point2.x, global_d.point2.y);
   //   double dist;
   //   Point p1, p2;
 
@@ -139,13 +192,19 @@ int main(int argc, char **argv) {
   //       global_d.point2 = p2;
   //     }
   //   }
-  // }
+  }
   
   // Free memory
   free(displs);
   free(scounts);
-  free(local_points.points);
-  free(point_vec.points);
+  // free(local_points.points);
+  // free(point_vec.points);
+
+  total_time += MPI_Wtime();
+  if (my_rank == 0) {
+    printf("Total time: %f seconds\n", total_time);
+    printf("\tReading time: %f seconds\n", read_time);
+  }
 
   MPI_Finalize();
   return 0;
