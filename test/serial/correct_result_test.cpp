d6413a8e49f9d5150f826fc87e2d9fc8079062ca
civts <giacomo.zanolli@studenti.unitn.it>
2023-01-03 18:31:08 +0100

    Add first real test of algorithm's correctness 🚨

--- test/serial/correct_result_test.cpp
+++ test/serial/correct_result_test.cpp
@@ -1,10 +1,37 @@
 #include <gtest/gtest.h>
 
-extern "C"{
-  // #include "../../src/serial/main.c"
+extern "C" {
+#include "../../src/parallel/utils/points.h"
+#include "../../src/serial/divide_2.c"
 }
 
+// Testing that it finds the right closest point in a small dataset
 TEST(Serial, CorrectResult) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+  PointVec input;
+  const int size = 8;
+  input.length = size;
+  input.points = (Point *)malloc(size * sizeof(Point));
+
+  input.points[0] = {1, 2};
+  input.points[1] = {-2, -3};
+  input.points[2] = {3, 4};
+  input.points[3] = {9, 5};
+  input.points[4] = {5, 6};
+  input.points[5] = {5, 5};
+  input.points[6] = {0, 1};
+  input.points[7] = {7, 7};
+
+  PairOfPoints result = closestPoints(input);
+  PairOfPoints expectedResult;
+  expectedResult.point1 = {5, 5};
+  expectedResult.point2 = {5, 6};
+  expectedResult.distance = 1.0;
+
+  EXPECT_EQ(result.distance, expectedResult.distance);
+
+  EXPECT_EQ(result.point1.x, expectedResult.point1.x);
+  EXPECT_EQ(result.point1.y, expectedResult.point1.y);
+
+  EXPECT_EQ(result.point2.x, expectedResult.point2.x);
+  EXPECT_EQ(result.point2.y, expectedResult.point2.y);
 }
